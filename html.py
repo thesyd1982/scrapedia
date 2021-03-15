a4704b4569e4f5d1c94b6ef69a9cef44185aa2db
thsyd1982 <salah.yacine.douakha@gmail.com>
2021-03-15 11:52:50 +0100

    my first commit

---
+++ html.py
@@ -0,0 +1,27 @@
+import httplib2
+import re
+
+class HTTPConnection:
+    """ Gére une instance de connection Http"""
+    def __init__(self, langue):
+        self.langue = langue
+
+
+
+    def get(self, page):
+        """ Retrouve une page"""
+        h = httplib2.Http(".cache")
+
+        self.url = "https://{0}.wikipedia.org/wiki/{1}".format(self.langue, page.replace(' ', '_'))
+
+        response, content = h.request(self.url, "GET")
+        codage= response['content-type'].split('=')[1]
+        print(s)
+
+        # cles = ['content-location','age','status','last-modified']
+        # res = {
+        #     c: response[c]for c in cles
+        # }
+        # print(res)
+        self.page = content.decode(codage)
+        print(self.page)
---
+++ main.py
@@ -0,0 +1,4 @@
+from html import HTTPConnection
+
+if __name__ == '__main__':
+    HTTPConnection("fr").get("bale")
